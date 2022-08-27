3da28e09effc401665c9f514872ddf8d3515a7de
Muhtasim Ferdous Reshad <reshad5646@gmail.com>
2022-08-28 05:16:35 +0600

    new1

---
+++ EzTechReview/Controllers/ReviewDetailsController.cs
@@ -0,0 +1,36 @@
+﻿using EzTechReview.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EzTechReview.Controllers
+{
+    public class ReviewDetailsController : Controller
+    {
+
+        private EztechreviewEntities db = new EztechreviewEntities();
+
+
+        // GET: ReviewDetails
+        public ActionResult ReviewDetails()
+        {
+
+
+            /* 
+             if (id == null)
+             {
+                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+             }
+             Review review = db.Reviews.Find(id);
+             if (review == null)
+             {
+                 return HttpNotFound();
+             }
+             return View(review);
+             */
+            return View();
+        }
+    }
+}
